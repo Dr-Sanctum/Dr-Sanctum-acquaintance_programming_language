05e2e8568441ec25ba9dab47b3f9d6bf7b9ed701
Антон Ефимов <Di_oXid@mail.ru>
2022-08-06 15:17:13 +0500

    Add Lesson5

---
+++ Lesson5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string UserName = Console.ReadLine();
+if (UserName.ToLower() == "маша")
+{
+    Console.Write("Привет СУПЕРМАША");
+}
+else
+{
+    Console.Write("Привет " + UserName);
+}
