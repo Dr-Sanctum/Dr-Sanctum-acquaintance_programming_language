b72d264f7b2f016a165ebc8e2f91702715074a8d
Антон Ефимов <Di_oXid@mail.ru>
2022-08-06 15:01:04 +0500

    Lesson modifed Random

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -1,4 +1,6 @@
-﻿int number1 =3;
-int number2 =4;
+﻿int number1 = new Random().Next(1,10);//Случайное число от 1 до 10
+int number2 = new Random().Next(2,15);//Случайное число от 2 до 15;
 int result = number1 + number2;
-Console.WriteLine (result);
+Console.WriteLine ("Первое число = " + number1);
+Console.WriteLine ("Второе число = " + number2);
+Console.WriteLine ("Сумма = " + result);
