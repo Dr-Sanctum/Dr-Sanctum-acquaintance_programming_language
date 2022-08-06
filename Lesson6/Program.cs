cf045c247e6b51135d473fb9bba6e31030dd062a
Антон Ефимов <Di_oXid@mail.ru>
2022-08-06 15:38:08 +0500

    Add Lesson6

---
+++ Lesson6/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] setOfNumber = new int[5];
+
+for (int i = 0; i < setOfNumber.Length; i++)
+{
+    setOfNumber[i] = new Random().Next(1,10);
+}
+
+foreach (var item in setOfNumber)
+{
+   Console.Write(item);
+}
+
+int max = setOfNumber[0];
+
+foreach (var item in setOfNumber)
+{
+    if (max < item)
+    {
+        max = item;
+    }
+}
+   Console.WriteLine("");
+   Console.WriteLine(max);
