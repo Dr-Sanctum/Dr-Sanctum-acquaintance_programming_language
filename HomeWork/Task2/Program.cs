f68e1d0299b7de3e66e0fc96ef58ac8fb91ca034
Антон Ефимов <Di_oXid@mail.ru>
2022-08-07 18:13:49 +0500

    Add HomeWork Task2

---
+++ HomeWork/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿string  firstDayoOfTheWeek = "Понедельник",
+        secondDayoOfTheWeek = "Вторник",
+        thirdDayoOfTheWeek = "Среда",
+        fourthDayoOfTheWeek = "Четверг",
+        fifthDayoOfTheWeek = "Пятница",
+        sixthDayoOfTheWeek = "Суббота",
+        seventhDayoOfTheWeek = "Воскресенье";
+
+Console.Write("Введите число от одного до семи ");
+int numberDayOfTheWeek = int.Parse(Console.ReadLine()); //От одного до семи
+
+if (numberDayOfTheWeek == 1)
+{
+    Console.Write("Номер дня недели - " + numberDayOfTheWeek + " Название дня недели - " + firstDayoOfTheWeek);
+}
+
+if (numberDayOfTheWeek == 2)
+{
+    Console.Write("Номер дня недели - " + numberDayOfTheWeek + " Название дня недели - " + secondDayoOfTheWeek);
+}
+
+if (numberDayOfTheWeek == 3)
+{
+    Console.Write("Номер дня недели - " + numberDayOfTheWeek + " Название дня недели - " + thirdDayoOfTheWeek);
+}
+
+if (numberDayOfTheWeek == 4)
+{
+    Console.Write("Номер дня недели - " + numberDayOfTheWeek + " Название дня недели - " + fourthDayoOfTheWeek);
+}
+
+if (numberDayOfTheWeek == 5)
+{
+    Console.Write("Номер дня недели - " + numberDayOfTheWeek + " Название дня недели - " + fifthDayoOfTheWeek);
+}
+
+if (numberDayOfTheWeek == 6)
+{
+    Console.Write("Номер дня недели - " + numberDayOfTheWeek + " Название дня недели - " + sixthDayoOfTheWeek);
+}
+
+if (numberDayOfTheWeek == 7)
+{
+    Console.Write("Номер дня недели - " + numberDayOfTheWeek + " Название дня недели - " + seventhDayoOfTheWeek);
+}
