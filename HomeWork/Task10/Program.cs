f756c8cf76dcdf86c37991b500200de070b724af
Антон Ефимов <Di_oXid@mail.ru>
2022-08-07 20:20:13 +0500

    Add HomeWork Task10

---
+++ HomeWork/Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задание: Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Задайте число ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число для проверки кратности заданному ");
+int b = int.Parse(Console.ReadLine());
+
+if ( b%a == 0)
+{
+    Console.WriteLine("Число " + b + " кратно числу " + a);
+}
+else
+{
+    Console.WriteLine("Число " + b + " не кратно числу " + a + ", остаток от деления = " + b%a);
+}
+
+
