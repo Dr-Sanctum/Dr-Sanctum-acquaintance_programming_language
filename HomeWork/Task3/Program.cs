67a6f3a129fe7a65e0b909fbfb809881ddaf7e35
Антон Ефимов <Di_oXid@mail.ru>
2022-08-07 18:26:51 +0500

    Add HomeWork Task3

---
+++ HomeWork/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 10, b = 5, c = 7;
+
+if (a > b && a > c)
+{
+    Console.WriteLine("Самое большое число = "+ a);
+}
+
+if (b > a && b > c)
+{
+    Console.WriteLine("Самое большое число = "+ b);
+}
+
+if (c > b && c > a)
+{
+    Console.WriteLine("Самое большое число = "+ c);
+}
