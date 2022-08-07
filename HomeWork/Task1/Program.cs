31ac80b82ea979ddefce83f8aabdb024638f3bc8
Антон Ефимов <Di_oXid@mail.ru>
2022-08-07 17:49:50 +0500

    Add HomeWork Task1

---
+++ HomeWork/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int a =25;
+int b = 5;
+
+Console.WriteLine("Первое число = " + a);
+Console.WriteLine("Второе число = " + b);
+
+if (a == b*b)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else 
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
