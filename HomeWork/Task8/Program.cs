4e33ea6af4767b223cd4fb577d03ab36ae8f071a
Антон Ефимов <Di_oXid@mail.ru>
2022-08-07 19:59:18 +0500

    Add HomeWork Task8

---
+++ HomeWork/Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задание: Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int x = new Random().Next(10,99);
+Console.WriteLine("Дано число " + x);
+
+int firstNumber = (x-x%10)/10;
+int secondNumber = x%10;
+
+if (firstNumber >= secondNumber)
+{
+    Console.WriteLine("Наибольшая цифра числа = " + firstNumber);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра числа = " + secondNumber);
+}
