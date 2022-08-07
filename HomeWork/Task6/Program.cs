31cc6969a42c6bd67545a02c3c380f81c8ed23de
Антон Ефимов <Di_oXid@mail.ru>
2022-08-07 19:45:53 +0500

    Add HomeWork Task7

--- HomeWork/Task6/Program.cs
+++ HomeWork/Task6/Program.cs
@@ -2,4 +2,4 @@
 Console.Write("Введите трёхзначное число ");
 int a = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Последняя цыфра введённого числа = " + a%100%10);
+Console.WriteLine("Последняя цыфра введённого числа = " + a%10);
