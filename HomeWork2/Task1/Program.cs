e5e698893576dbb76fe4b4a84ced4014d58c3bca
Антон Ефимов <Di_oXid@mail.ru>
2022-08-11 19:44:40 +0500

    Add HomeWork2 Task1

---
+++ HomeWork2/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задание: Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+void Holiday(int numberDayWeek){
+if (numberDayWeek  == 6 || numberDayWeek == 7)
+{
+    Console.Write("День недели выходной");
+} 
+if (1 <= numberDayWeek && numberDayWeek < 6 )
+{
+    Console.Write("День недели будни");
+}
+}
+
+
+Console.Write("Введите число от 1 до 7 ");
+
+Holiday(int.Parse(Console.ReadLine()));
