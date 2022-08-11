a9263371c22a33d26d62d75529ce25ccb75bcef4
Антон Ефимов <Di_oXid@mail.ru>
2022-08-11 20:16:27 +0500

    Add HomeWork2 Task2

---
+++ HomeWork2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задание: По двум заданным числам проверять является ли одно квадратом другого (с помощью функции)
+
+string checkSqare(int firstNumber, int secondNumber)
+{
+if (firstNumber == secondNumber*secondNumber)
+{
+    return "Первое число является квадратом второго";
+}
+else 
+{
+    return "Первое число не является квадратом второго";}
+}
+
+
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите первое число ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine( checkSqare(a, b));
+
