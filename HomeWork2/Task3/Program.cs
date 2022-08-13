fe1b329f96487e8ecea79c372d5c154f339695d8
Антон Ефимов <Di_oXid@mail.ru>
2022-08-13 18:13:45 +0500

    Add HomeWork2 Task3

---
+++ HomeWork2/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задание: Задать номер четверти, показать диапазоны для возможных координат
+
+void RangeQuarter (int numberQuarter) 
+{
+
+if (numberQuarter == 1) //Первая четверть
+{
+    Console.WriteLine("Диапазон чертверти(1): X от >0 до inf, Y от >0 до inf");
+}
+
+if (numberQuarter == 2) //Вторая четверть
+{
+    Console.WriteLine("Диапазон чертверти(2): X от <0 до -inf, Y от >0 до inf");
+}
+
+if (numberQuarter == 3) //Третья четверть
+{
+    Console.WriteLine("Диапазон чертверти(3): X от <0 до -inf, Y от <0 до -inf");
+}
+
+if (numberQuarter == 4) //Четвёртая
+{
+    Console.WriteLine("Диапазон чертверти(4): X от >0 до inf, Y от <0 до -inf");
+}
+
+}
+
+
+
+Console.Write("Введите номер четверти системы координат (от 1 до 4) ");
+int a = int.Parse(Console.ReadLine());
+
+RangeQuarter (a);
+
