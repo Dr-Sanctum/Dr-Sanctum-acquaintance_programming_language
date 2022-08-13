9c09a5224c7c5c9d637bdada536162b6d5ebfc9c
Антон Ефимов <Di_oXid@mail.ru>
2022-08-13 19:36:19 +0500

    Add HomeWork2 Task9

---
+++ HomeWork2/Task9/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задание: Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите последнее число для умножения предыдущих чисел ");
+
+int a = int.Parse(Console.ReadLine());
+int i = 1;
+double result = 1;
+while (i <= a)
+{
+    result = result * i;
+    
+    i++;
+}
+Console.Write("Произведение чисел до (факториал) " + a + " = " + result);
