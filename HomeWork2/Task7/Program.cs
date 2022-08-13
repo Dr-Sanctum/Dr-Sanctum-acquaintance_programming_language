990c36bcd361e336bdb8cebd66d880b149970b5e
Антон Ефимов <Di_oXid@mail.ru>
2022-08-13 19:12:03 +0500

    Add HomeWork2 Task7

---
+++ HomeWork2/Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задание: Определить количество цифр в числе
+
+Console.Write("Введите число ");
+
+int a = int.Parse(Console.ReadLine());
+
+int result = a;
+int i = 1;
+
+while (result >= 10)
+{
+    result = result/10;
+    
+    i++;
+}
+Console.Write("В числе " + a + " количество цифр = " + i);
