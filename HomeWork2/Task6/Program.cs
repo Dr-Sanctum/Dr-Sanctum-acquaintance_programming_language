c2e1e7ac4b0548f3814d32a4dca9abe4f2259949
Антон Ефимов <Di_oXid@mail.ru>
2022-08-13 19:03:20 +0500

    Add HomeWork2 Task6

---
+++ HomeWork2/Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задание: Найти сумму чисел от 1 до А
+
+Console.Write("Введите последнее число для суммирования предыдущих чисел ");
+
+int a = int.Parse(Console.ReadLine());
+int i = 1;
+int result = 0;
+while (i <= a)
+{
+    result += i;
+    
+    i++;
+}
+Console.Write("Сумма чисел до " + a + " = " + result);
