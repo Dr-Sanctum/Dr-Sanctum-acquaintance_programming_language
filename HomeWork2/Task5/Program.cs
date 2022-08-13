e243ead9d625b7e4c45f2b517d83243db4700234
Антон Ефимов <Di_oXid@mail.ru>
2022-08-13 18:56:32 +0500

    Add HomeWork2 Task5

---
+++ HomeWork2/Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задание: Найти кубы чисел от 1 до N
+
+Console.Write("Введите ограничение выводимых кубов чисел ");
+
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= n)
+{
+    Console.Write(Math.Pow(i, 3)+ " ");
+    i++;
+}
