1fd164d8b6f95d8245cbea672645da890be5158b
Антон Ефимов <Di_oXid@mail.ru>
2022-08-13 19:22:29 +0500

    Add HomeWork2 Task8

---
+++ HomeWork2/Task8/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задание: Подсчитать сумму цифр в числе
+
+Console.Write("Введите число ");
+
+int a = int.Parse(Console.ReadLine());
+
+int b = a;
+int result = a%10;
+
+if (a > 9)
+{
+    while (b >= 10)
+    {
+        b = b/10;
+        result += b%10;
+    }
+
+     Console.Write("В числе " + a + " сумма цифр = " + result);
+}
+if (0 < b && b > 10)
+{
+    Console.Write("В числе " + a + " сумма цифр = " + b);
+}
