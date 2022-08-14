18bb50ab902e10dc6d07320bd9ddb456dff17d42
Антон Ефимов <Di_oXid@mail.ru>
2022-08-14 22:15:46 +0500

    Add HomeWork3 Task7

---
+++ HomeWork3/Task7/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задание: Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] array = new int[10];
+
+void FillAndViewArray( int[] _array)
+{
+for (int i = 0; i < _array.Length; i++)
+    {
+        _array[i] = new Random().Next(0,10);
+    }
+
+for (int i = 0; i < _array.Length; i++)
+    {
+        Console.Write($"{_array[i]} ");
+    }
+}
+
+void nonEvenNumberIndexSum(int[] _array)
+{
+    int nonEvenNumberIndexSum = 0;
+ for (int i = 1; i < _array.Length; i+=2)
+ {
+    nonEvenNumberIndexSum += _array[i];
+ }
+
+Console.WriteLine();
+Console.WriteLine("Cумма чисел массива с нечётным индексом = " + nonEvenNumberIndexSum);
+}
+
+FillAndViewArray(array);
+
+nonEvenNumberIndexSum(array);
