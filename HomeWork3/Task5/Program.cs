4e6ca597d449cbb7c6fdebe64b753056a5ee48f9
Антон Ефимов <Di_oXid@mail.ru>
2022-08-14 21:54:40 +0500

    Add HomeWork3 Task5

---
+++ HomeWork3/Task5/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задание: Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int[] array = new int[10];
+
+void FillAndViewArray( int[] _array)
+{
+for (int i = 0; i < _array.Length; i++)
+    {
+        _array[i] = new Random().Next(0,1000);
+    }
+
+for (int i = 0; i < _array.Length; i++)
+    {
+        Console.Write($"{_array[i]} ");
+    }
+}
+
+void Counter(int[] _array)
+{
+    int evenNumberedCounter = 0;
+    int nonEvenNumberedCounter = 0;
+ foreach (var item in _array)
+ {
+    if (item%2 == 0)
+    {
+        evenNumberedCounter++;
+    }
+    else 
+    {
+        nonEvenNumberedCounter++;
+    }
+ }
+
+Console.WriteLine();
+Console.WriteLine("Количество чётных элементов = " + evenNumberedCounter);
+Console.WriteLine("Количество нечётных элементов = " + nonEvenNumberedCounter);
+}
+
+FillAndViewArray(array);
+
+Counter(array);
