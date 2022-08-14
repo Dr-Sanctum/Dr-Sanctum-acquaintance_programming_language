3ba85f3ebbb01478687fda6ff8fb77aa84a4df8c
Антон Ефимов <Di_oXid@mail.ru>
2022-08-14 22:45:04 +0500

    Add HomeWork3 Task9

---
+++ HomeWork3/Task9/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задание: В указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+double[] array = new double[10];
+
+void FillAndViewArray( double[] _array)
+{
+for (int i = 0; i < _array.Length; i++)
+    {
+        _array[i] = new Random().NextDouble()*10;
+    }
+
+for (int i = 0; i < _array.Length; i++)
+    {
+        Console.Write($"{_array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Diff(double[] _array)
+{
+    double min = _array[0];
+    double max = _array[0];
+ for (int i = 1; i < _array.Length; i++)
+ {
+    if (_array[i]> max)
+    {
+        max = _array[i];
+    }
+    if (_array[i]< min)
+    {
+        min = _array[i];
+    }
+ }
+ Console.WriteLine("Минимальное = " + min);
+ Console.WriteLine("Максимальное = " + max);
+ Console.WriteLine("Разница = " + (max - min));
+}
+
+FillAndViewArray(array);
+
+Diff(array);
