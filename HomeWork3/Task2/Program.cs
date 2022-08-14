c40c5c7a5ab2af37798809bbc784a5097c215d9d
Антон Ефимов <Di_oXid@mail.ru>
2022-08-14 20:57:07 +0500

    Add HomeWork3 Task2

---
+++ HomeWork3/Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задание: Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму чётных/нечётных элементов массива
+
+int[] array = new int[12];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,9);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+
+int SumPositive(int[] _array)
+{
+int result = 0;
+
+foreach (var item in _array)
+{
+    if (item%2 ==0)
+    {
+        result += item;
+    }
+}
+
+return result;
+}
+
+int SumNegative(int[] _array)
+{
+int result = 0;
+
+foreach (var item in _array)
+{
+    if (item%2 == 1)
+    {
+        result += item;
+    }
+}
+return result;
+}
+
+Console.WriteLine();
+Console.WriteLine("Сумма всех чётных чисел массива = " + SumPositive(array));
+Console.WriteLine("Сумма всех нечётных чисел массива = " + SumNegative(array));
