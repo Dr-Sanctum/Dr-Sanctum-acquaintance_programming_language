bfde3d76f37c4b977b3682085ed78f4877f6c27b
Антон Ефимов <Di_oXid@mail.ru>
2022-08-18 21:57:08 +0500

    Add HomeWork4 Task1

---
+++ HomeWork4/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задание: Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+
+void CrossPoint()
+{
+int k1 = 10;
+int k2 = 0;
+int b1 = 1;
+int b2 = -3;
+
+double x = (b2-b1)/(k1-k2);
+double y = k1*(b2-b1)/(k1-k2)+b1;
+
+Console.WriteLine($"Точка пересечения прямых - ({x},{y})");
+}
+
+CrossPoint();
