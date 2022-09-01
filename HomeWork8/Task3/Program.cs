3de2864d57e4bdd98ff6d13dc122b3366ec33bf5
Антон Ефимов <Di_oXid@mail.ru>
2022-09-01 17:43:21 +0500

    Add HomeWork8 Task3

---
+++ HomeWork8/Task3/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задание 3: Сформировать трехмерный массив не повторяющимися двузначными 
+// числами показать его построчно на экран выводя индексы соответствующего элемента
+
+void PrintArray(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"Индекс - ({i},{j},{k}) {matrix[i, j, k]}\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+void Foo(int[,,] matrix)
+{
+    var rand = new Random();
+    int tempRandom;
+    int[] temp = new int[88];// двузначные числа от 10 до 99
+    int counter = 0;
+    bool x = true;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                for (; ; )
+                {
+                    x = true;
+                    tempRandom = rand.Next(10, 100); // двузначные числа от 10 до 99
+                    for (int z = 0; z < temp.Length; z++)
+                    {
+                        if (tempRandom == temp[z])
+                        {
+                            x = false;
+                            break;
+                        }
+                    }
+                    if (x)
+                    {
+                        temp[counter] = tempRandom;
+                        counter++;
+                        break;
+                    }
+                }
+                matrix[i, j, k] = tempRandom;
+            }
+        }
+    }
+}
+Console.Clear();
+
+int m, n, l;
+Console.Write("Введите количество строк ");
+m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов ");
+n = int.Parse(Console.ReadLine());
+Console.Write("Введите количество блоков строка-столбец ");
+l = int.Parse(Console.ReadLine());
+
+int[,,] array = new int[m, n, l];
+Console.WriteLine("Трёхмерный массив:");
+Foo(array);
+PrintArray(array);
