aaa5630fb31dd4739d79e0ee9363f70089b37a76
Антон Ефимов <Di_oXid@mail.ru>
2022-09-04 14:34:07 +0500

    Add HomeWork9 Task6

---
+++ HomeWork9/Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задание 6: Написать программу возведения числа А в целую стень B (рекурсия)
+
+int Foo(int a, int b)
+{
+    
+   
+    if (b > 1)
+    {
+        return a  = Foo(a , b - 1)*a;
+    }
+    return b == 0 ? 1 : a;
+}
+
+Console.WriteLine(Foo(2,0));
