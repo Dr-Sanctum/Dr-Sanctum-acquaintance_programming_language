768e5ac2e3e023acb35b75eb36d02a034d394e92
Антон Ефимов <Di_oXid@mail.ru>
2022-09-04 11:14:45 +0500

    Add HomeWork9 Task3

---
+++ HomeWork9/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задание 3: Найти сумму элементов от M до N, N и M заданы
+
+int Foo(int m, int n)
+{
+    int sum = + m;
+    if (m < n)
+    {
+        return sum += Foo(m + 1, n);
+    }
+    else
+    {
+        return sum;
+    }
+}
+
+Console.WriteLine(Foo(1, 5));
