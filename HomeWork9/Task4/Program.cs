9b09ffc346cf856db21b440abe79e3c5d9b2f4bb
Антон Ефимов <Di_oXid@mail.ru>
2022-09-04 11:55:12 +0500

    Add HomeWork9 Task4

---
+++ HomeWork9/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задание 4: Найти сумму цифр числа (рекурсия)
+
+int Foo(int n)
+{
+    int sum = 0;
+    if (n > 10)
+    {
+        
+        return sum + n%10 + Foo(n/10);
+    }
+    else
+    {
+        return sum + n;   
+    }
+
+}
+
+Console.WriteLine(Foo(44656));
