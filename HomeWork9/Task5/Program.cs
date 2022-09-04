21c85da7358886662a64e07140ec9be89c9a5093
Антон Ефимов <Di_oXid@mail.ru>
2022-09-04 12:15:07 +0500

    Add HomeWork9 Task5

---
+++ HomeWork9/Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задание 5: Написать программу вычисления функции Аккермана
+
+int Foo(int n, int m)
+{
+    if (n == 0)
+    {
+       return m + 1;
+    }
+    else
+    {
+        if (n != 0 && m == 0)
+        {
+           return Foo(n - 1, 1);
+        }
+        else
+        {
+               return Foo(n - 1, Foo(n, m - 1));
+        }
+    }
+
+
+}
+
+Console.WriteLine(Foo(4, 3));
